45cacd052caa879a1340a41d3274d0a631b0997f
Rubén <rumube948@gmail.com>
2021-12-05 12:47:43 +0100

    SpaceGeneration and Destroy

--- SpaceAdventure/Assets/Scripts/WorldGeneration/PerlinNoise.cs
+++ SpaceAdventure/Assets/Scripts/WorldGeneration/PerlinNoise.cs
@@ -4,58 +4,91 @@ using UnityEngine;
 
 public class PerlinNoise : MonoBehaviour
 {
-    // Width and height of the texture in pixels.
-    public int pixWidth;
-    public int pixHeight;
-
-    // The origin of the sampled area in the plane.
-    public float xOrg;
-    public float yOrg;
-
-    // The number of cycles of the basic noise pattern that are repeated
-    // over the width and height of the texture.
+    [Header("Game Parameters")]
     public float scale = 1.0F;
-
-    private Texture2D noiseTex;
-    private Color[] pix;
-    private Renderer rend;
+    private GameObject _ship;
+    public float _range;
+    public float _spawnRate;
+    public GameObject _asteroid;
+    public Vector3 _lastPos;
 
     void Start()
     {
-        rend = GetComponent<Renderer>();
-
-        // Set up the texture and a Color array to hold pixels during processing.
-        noiseTex = new Texture2D(pixWidth, pixHeight);
-        pix = new Color[noiseTex.width * noiseTex.height];
-        rend.material.mainTexture = noiseTex;
+        _ship = GameObject.FindGameObjectWithTag("Player");
+        CalcNoise();
     }
 
     void CalcNoise()
     {
-        // For each pixel in the texture...
-        float y = 0.0F;
+        /*
+        GameObject[] asteroidList = GameObject.FindGameObjectsWithTag("Asteroid");
 
-        while (y < noiseTex.height)
+        foreach (GameObject currentAsteroid in asteroidList)
         {
-            float x = 0.0F;
-            while (x < noiseTex.width)
-            {
-                float xCoord = xOrg + x / noiseTex.width * scale;
-                float yCoord = yOrg + y / noiseTex.height * scale;
-                float sample = Mathf.PerlinNoise(xCoord, yCoord);
-                pix[(int)y * noiseTex.width + (int)x] = new Color(sample, sample, sample);
-                x++;
-            }
-            y++;
+            Destroy(currentAsteroid);
         }
+        */
+        for (int x = (int)(_ship.transform.position.x - _range); x < _ship.transform.position.x + _range; x++)
+        {
+            for (int y = (int)(_ship.transform.position.y - _range); y < _ship.transform.position.y + _range; y++)
+            {
+                for (int z = (int)(_ship.transform.position.z - _range); z < _ship.transform.position.z + _range; z++)
+                {
+                    if (Perlin3D(x, y, z) <= _spawnRate)
+                    {
+                        GameObject newAsteroid = Instantiate(_asteroid, transform);
+                        newAsteroid.transform.position = new Vector3(x, y, z);
+                    }
+                }
+            }
+        }
+        _lastPos = _ship.transform.position;
+    }
 
-        // Copy the pixel data to the texture and load it into the GPU.
-        noiseTex.SetPixels(pix);
-        noiseTex.Apply();
+    public float Perlin3D(float x, float y, float z)
+    {
+        x *= scale;
+        y *= scale;
+        z *= scale;
+
+        float AB = Mathf.PerlinNoise(x, y);
+        float BC = Mathf.PerlinNoise(y, z);
+        float AC = Mathf.PerlinNoise(x, z);
+
+        float BA = Mathf.PerlinNoise(y, x);
+        float CB = Mathf.PerlinNoise(z, y);
+        float CA = Mathf.PerlinNoise(z, x);
+
+        float ABC = AB + BC + AC + BA + CB + CA;
+        return ABC / 6f;
+
     }
 
     void Update()
     {
-        CalcNoise();
+        if (NeedUpdate())
+            CalcNoise();
+    }
+
+    bool NeedUpdate()
+    {
+        bool result = false;
+        //X LIMIT
+        float xLimitSup = _lastPos.x + (_range / 2);
+        float xLimitInf = _lastPos.x - (_range / 2);
+        //Y LIMIT
+        float yLimitSup = _lastPos.y + (_range / 2);
+        float yLimitInf = _lastPos.y - (_range / 2);
+        //Z LIMIT
+        float zLimitSup = _lastPos.z + (_range / 2);
+        float zLimitInf = _lastPos.z - (_range / 2);
+
+        Vector3 currentPos = _ship.transform.position;
+
+        if (currentPos.x >= xLimitSup || currentPos.x <= xLimitInf || currentPos.y >= yLimitSup || currentPos.y <= yLimitInf || currentPos.z >= zLimitSup || currentPos.z <= zLimitInf)
+            result = true;
+
+
+        return result;
     }
 }
