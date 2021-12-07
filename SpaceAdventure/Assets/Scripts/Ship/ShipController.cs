af7c0ac192a2f48ca6e3af250beaa95a41b0a886
Rumube <rumube@floridauniversitaria.es>
2021-12-07 13:57:55 +0100

    Update Controles

--- SpaceAdventure/Assets/Scripts/Ship/ShipController.cs
+++ SpaceAdventure/Assets/Scripts/Ship/ShipController.cs
@@ -9,8 +9,13 @@ public class ShipController : MonoBehaviour
     Rigidbody _rb;
 
     [Header("Parametros Movimiento")]
-    public int[] _velocity = new int[5];
-    public int _currentVelocity = 0;
+    public float[] _velocity = new float[5];
+    public int _currentVelocity = 1;
+    public int _currentInc = 0;
+    public float _currentGiro = 0;
+    public float _velRotation;
+    public float _velGiro;
+    int _giro;
 
 
     // Start is called before the first frame update
@@ -23,18 +28,42 @@ public class ShipController : MonoBehaviour
     void Update()
     {
         UpdateControllData();
+        CalculeGiro();
         MoveShip();
         Client.Instance.SendShipData(gameObject);
+    }
+
+    void CalculeGiro()
+    {
+        switch (_giro)
+        {
+            case -1:
+                //Izquierda
+                _currentGiro = transform.rotation.eulerAngles.y - 10;
+                break;
+            case 0:
+                //No girar
+
+                break;
+            case 1:
+                //Derecha
+                _currentGiro = transform.rotation.eulerAngles.y + 10;
+                break;
+        }
     }
 
     void UpdateControllData()
     {
         _currentVelocity = Client.Instance._controllData.currentVel;
+        _currentInc = Client.Instance._controllData.currentInc;
+        _giro = Client.Instance._controllData.giro;
     }
 
     void MoveShip()
     {
         _rb.AddRelativeForce(Vector3.forward * _velocity[_currentVelocity] * Time.deltaTime, ForceMode.Impulse);
+        Quaternion newQ = Quaternion.Euler(_currentInc, _currentGiro, transform.eulerAngles.z);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newQ, Time.deltaTime * _velRotation);
     }
 
     public void setCurrentVelocity(int value)
